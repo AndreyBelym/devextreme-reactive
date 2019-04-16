72b181d69b43fdef4f124a2ae4bc5fb59785e869
Andrey Belym <belym.a.2105@gmail.com>
2019-04-16 18:12:04 +0300

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,9 +1,11 @@
 ./node_modules/.bin/testcafe -v
-./node_modules/.bin/testcafe "chrome:userProfile:headless:cdpPort=9222 --remote-debugging-port=9222" --hostname localhost --ports 1337,1338 &
+./node_modules/.bin/testcafe "chrome:userProfile:headless:cdpPort=9222" --hostname localhost --ports 1337,1338 &
 sleep 5
 
 echo Start Browser
 # firefox -headless http://localhost:1337/browser/connect &
 # google-chrome --headless --remote-debugging-port=9222 http://localhost:1337/browser/connect &
-wait %1
+sleep 15
+
+ps -ejf | grep chrome
 # node -e "require('chrome-remote-interface')().then(() => console.log('OK'))"
