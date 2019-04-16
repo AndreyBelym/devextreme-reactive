83ed7148d912f21d8904d683416feaca16589d8c
Andrey Belym <belym.a.2105@gmail.com>
2019-04-16 17:53:48 +0300

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,5 +1,5 @@
 ./node_modules/.bin/testcafe -v
-./node_modules/.bin/testcafe "chrome:headless:userProfile" --hostname localhost --ports 1337,1338 &
+./node_modules/.bin/testcafe "chrome:headless:userProfile:cdpPort=9222 --remote-debugging-port=9222" --hostname localhost --ports 1337,1338 &
 sleep 5
 echo Start Browser
 # firefox -headless http://localhost:1337/browser/connect &
