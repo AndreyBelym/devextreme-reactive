2b823ab0cea81a9098045f49424c4c80bc741bab
Andrey Belym <belym.a.2105@gmail.com>
2019-04-16 18:31:10 +0300

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,5 +1,6 @@
+chmod +x /home/circleci/project/chrome.sh
 ./node_modules/.bin/testcafe -v
-./node_modules/.bin/testcafe "chrome:/usr/bin/google-chrome:headless --disable-setuid-sandbox --no-sandbox --speed aa" --hostname localhost --ports 1337,1338 &
+./node_modules/.bin/testcafe "chrome:/home/circleci/project/chrome.sh:headless --disable-setuid-sandbox --no-sandbox --speed aa" --hostname localhost --ports 1337,1338 &
 sleep 5
 
 echo Start Browser
@@ -7,4 +8,5 @@ echo Start Browser
 #google-chrome --headless --remote-debugging-port=9222 http://localhost:1337/browser/connect &
 ps -ejf | grep chrome
 wait %1
+cat log.txt
 # node -e "require('chrome-remote-interface')().then(() => console.log('OK'))"
