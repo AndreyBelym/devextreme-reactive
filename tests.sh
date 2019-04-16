1251b02ac91d3365ccdf1b1f6e17089c60e4e2f6
Andrey Belym <belym.a.2105@gmail.com>
2019-04-16 16:07:23 +0300

    Create tests.sh

---
+++ tests.sh
@@ -0,0 +1,4 @@
+testcafe remote --hostname localhost --ports 1337,1338 &
+echo Start Browser
+google-chrome --no-sandbox --headless --remote-debugging-port=9222 http://localhost:1337 &
+node -e "require('chrome-remote-interface')().then(() => console.log('OK'))"
