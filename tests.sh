81185a21ff60143c840eb5d807ec76b7400f6728
Andrey Belym <belym.a.2105@gmail.com>
2019-04-16 17:12:02 +0300

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,6 +1,7 @@
 ./node_modules/.bin/testcafe -v
-./node_modules/.bin/testcafe remote test.js --hostname localhost --ports 1337,1338 &
-sleep 10
+./node_modules/.bin/testcafe remote ./tests/**/*-testcafe.js --hostname localhost --ports 1337,1338 &
+yarn run build:watch &
+sleep 60
 echo Start Browser
 # firefox -headless http://localhost:1337/browser/connect &
 google-chrome --no-sandbox --headless --remote-debugging-port=9222 http://localhost:1337/browser/connect &
