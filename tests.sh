c9fd7d871043078b57b28bc71460bb225dd813a8
Andrey Belym <belym.a.2105@gmail.com>
2019-04-16 16:37:05 +0300

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,6 +1,6 @@
-./node_modules/.bin/testcafe remote test.js --hostname localhost --ports 1337,1338 &
+./node_modules/.bin/testcafe firefox:headless test.js --hostname localhost --ports 1337,1338 &
 sleep 10
 echo Start Browser
-google-chrome --no-sandbox --headless --remote-debugging-port=9222 http://localhost:1337/browser/connect &
+# google-chrome --no-sandbox --headless --remote-debugging-port=9222 http://localhost:1337/browser/connect &
 wait %1
 # node -e "require('chrome-remote-interface')().then(() => console.log('OK'))"
