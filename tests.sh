f84bdb4c650779a6b30ea163cc2db6dab4bd7970
Andrey Belym <belym.a.2105@gmail.com>
2019-04-16 18:19:14 +0300

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,11 +1,10 @@
 ./node_modules/.bin/testcafe -v
-./node_modules/.bin/testcafe remote --hostname localhost --ports 1337,1338 &
+./node_modules/.bin/testcafe "chrome:headless --disable-setuid-sandbox --no-sandbox" --hostname localhost --ports 1337,1338 &
 sleep 5
 
 echo Start Browser
 # firefox -headless http://localhost:1337/browser/connect &
-google-chrome --headless --remote-debugging-port=9222 http://localhost:1337/browser/connect &
-sleep 1
-
+#google-chrome --headless --remote-debugging-port=9222 http://localhost:1337/browser/connect &
 ps -ejf | grep chrome
+wait %1
 # node -e "require('chrome-remote-interface')().then(() => console.log('OK'))"
