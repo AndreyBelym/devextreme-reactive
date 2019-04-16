b74a8bc63cda1a23027cc6785fc4d4f3a4a97ef0
Andrey Belym <belym.a.2105@gmail.com>
2019-04-16 16:23:41 +0300

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -2,5 +2,5 @@
 sleep 10
 echo Start Browser
 google-chrome --no-sandbox --headless --remote-debugging-port=9222 http://localhost:1337/browser/connect &
-sleep 10
+wait 1
 # node -e "require('chrome-remote-interface')().then(() => console.log('OK'))"
